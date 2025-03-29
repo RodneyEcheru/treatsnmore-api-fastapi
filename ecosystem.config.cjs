aa4a3a696721ee3ffdbd5110716b96a28c24490a
RodneyEcheru <ivilleinc@gmail.com>
2025-03-29 11:23:00 +0300

    updated ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,17 @@
+
+module.exports = {
+  apps: [
+    {
+      name: 'treats-n-more-fastapi',
+      script: 'main.py',
+      interpreter: 'python3.12',
+      watch: true,
+      autorestart: true,
+      env: {
+        PORT: 9509, // Change this if needed
+        HOST: '0.0.0.0',  // Ensure it binds to all interfaces
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
